c55e7ea36d1af0e450fa8f2ce20afdbc4b4e32b7
pomelo <516110874@qq.com>
2015-06-14 09:35:38 +0800

    ASP.NET helloworld

---
+++ firstaspnet/firstaspnet/helloword.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace firstaspnet
+{
+    public partial class helloword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "hello world !!!";
+        }
+    }
+}
